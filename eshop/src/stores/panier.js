8d5e22f3235ea8e359cf38f12cf02b348311abc4
MelTripot <tripot.melanie@ynov.com>
2023-02-18 12:47:43 +0100

    total et payer work

--- eshop/src/stores/panier.js
+++ eshop/src/stores/panier.js
@@ -7,28 +7,49 @@ export const usePanierStore = defineStore("panier", () => {
     const i = artictelst.value.findIndex((x) => x.id == article.id);
     if (i != -1) {
       artictelst.value[i].quantity += 1;
+      artictelst.value[i].qtePrice =
+        artictelst.value[i].quantity * artictelst.value[i].price;
     } else {
       article.quantity = 1;
       artictelst.value.push(article);
     }
     console.log(artictelst.value);
   }
-  const count = computed(() => articlelst.value.length);
+  const count = computed(() => calculCount());
+  function calculCount() {
+    let tot = 0;
+    artictelst.value.forEach((x) => {
+      tot += x.quantity;
+    });
+    return tot;
+  }
   function changeQuantity(id, qte) {
-    const i = artictelst.value.findIndex((x) => x.id == id);
-    artictelst.value[i].quantity = qte;
-    console.log(artictelst.value[i].quantity);
-    if (artictelst.value[i].quantity < 1) {
-      console.log("ici" + artictelst.value[i].quantity);
-      delete (artictelst, i); // TODO voir le delete si on le replace pas par un set
+    //en vrai bloquer la descente pour pas descendre en dessous de 1 et forcer l'utilisation du bouton suppr
+    if (qte != 0) {
+      const i = artictelst.value.findIndex((x) => x.id == id);
+      artictelst.value[i].quantity = qte;
+      artictelst.value[i].qtePrice =
+        artictelst.value[i].quantity * artictelst.value[i].price;
     }
   }
-  function remove(id) {
-    //TODO remove
+  function removeItem(id) {
+    const i = artictelst.value.findIndex((x) => x.id == id);
+    artictelst = artictelst.value.splice(i, 1);
+    // delete artictelst.value[i];
+    console.log(artictelst.value);
+  }
+
+  const prixTot = computed(() => calculPrixtot());
+  function calculPrixtot() {
+    let tot = 0;
+    artictelst.value.forEach((x) => {
+      tot += x.quantity * x.price;
+    });
+    return tot;
   }
   function $reset() {
     artictelst = [];
   }
 
-  return { count, artictelst, add, changeQuantity };
+  return { count, artictelst, prixTot, add, removeItem, changeQuantity };
 });
--- eshop/src/views/PanierView.vue
+++ eshop/src/views/PanierView.vue
@@ -9,19 +9,62 @@
     <button @click="changeQuantity(a.id, a.quantity - 1)">-</button>
     {{ a.quantity }}
     <button @click="changeQuantity(a.id, a.quantity + 1)">+</button>
+    <img class="icon" @click="remove(a)" src="../assets/trash.png" />
+    <span> prix unitaire : {{ a.price }}</span>
+    <span> prix total : {{ a.qtePrice }}</span>
   </div>
+  Total panier : {{ total }}
+
+  <button class="payer" @click="payer">
+    <img class="iconPanier" src="../assets/panier.png" /> Payer
+  </button>
 </template>
 
 <script setup>
 import { usePanierStore } from "../stores/panier";
 const store = usePanierStore();
-console.log(store.artictelst);
 function changeQuantity(id, qte) {
   store.changeQuantity(id, qte);
 }
+function remove(article) {
+  store.removeItem(article.id);
+}
+const payer = () => {
+  store.$reset();
+};
+const total = store.prixTot;
 </script>
 <style scoped>
+.icon {
+  margin-top: 2rem;
+  max-width: 24px;
+}
 .panier {
   @apply;
+}
+.payer {
+  appearance: none;
+  background-color: #2ea44f;
+  border: 1px solid rgba(27, 31, 35, 0.15);
+  border-radius: 6px;
+  box-shadow: rgb(27 31 35 / 10%) 0 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system, system-ui, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  padding: 6px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+  white-space: nowrap;
 }
 </style>
